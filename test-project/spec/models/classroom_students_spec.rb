a7144e2527f8ae8d360980c5abd085e02db39e2d
Carol Nichols <cnichols@thinkthroughmath.com>
2012-12-04 16:21:22 -0500

    Adding a test that shows something that works along with a bunch of the has_many methods that don't work

--- test-project/spec/models/classroom_students_spec.rb
+++ test-project/spec/models/classroom_students_spec.rb
@@ -18,8 +18,28 @@ describe "Classroom and student relations" do
   end
 
   describe "classroom" do
-    it "has a student on the enrollment shard" do
-      @classroom.students.first.connection.object_id.should == Student.on_db(:enrollments).connection.object_id
+    it "has a student on the enrollment shard (workaround)" do
+      student_ids = @classroom.classroom_students.pluck(:student_id).uniq
+      classroom_students = Student.find(student_ids)
+
+      first_student = classroom_students.first
+      first_student.connection.object_id.should == Student.on_db(:enrollments).connection.object_id
+    end
+
+    it "has students" do
+      @classroom.students.should_not == []
+    end
+
+    it "has student_ids" do
+      @classroom.student_ids.should_not be_empty
+    end
+
+    it "can ask if it's empty" do
+      @classroom.students.empty?.should be_false
+    end
+
+    it "can ask the size" do
+      @classroom.students.size.should == 1
     end
   end
 end
