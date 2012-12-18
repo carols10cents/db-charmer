c0e22414e43d7714be0663e1cfda16f0155d9288
Carol Nichols <carol.nichols@gmail.com>
2012-12-18 18:29:44 -0500

    logging

--- test-project/spec/models/school_teachers_spec.rb
+++ test-project/spec/models/school_teachers_spec.rb
@@ -2,19 +2,28 @@ require 'spec_helper'
 
 describe "Schools and teachers - shard choice is dependent" do
   before(:each) do
-    ['one', 'two'].each do |shard|
-      School.shard_for(shard).delete_all
-      Teacher.shard_for(shard).delete_all
+    [:schools_shard_one, :schools_shard_two].each do |shard|
+      School.on_db(shard).delete_all
+      Teacher.on_db(shard).delete_all
     end
 
-    @school = School.shard_for('one').create!
+    @school = School.on_default_shard.create!
+
+    puts "school = #{@school.inspect}"
 
     # Some of these tests pass if we do this:
     # @teacher = @school.teachers.shard_for('1').create!
     # but we don't want to have to do that.
 
     # We expect to be able to do:
-    @teacher = @school.teachers.create!
+    @teacher = @school.teachers.on_default_shard.create!
+    puts "teacher = #{@teacher.inspect}"
+    puts "================"
+    puts "School Shard 1: #{School.on_db(:schools_shard_one).all.inspect}"
+    puts "Teacher Shard 1: #{Teacher.on_db(:schools_shard_one).all.inspect}"
+    puts "================"
+    puts "School Shard 1: #{School.on_db(:schools_shard_two).all.inspect}"
+    puts "Teacher Shard 1: #{Teacher.on_db(:schools_shard_one).all.inspect}"
   end
 
   describe "teacher" do
@@ -34,7 +43,7 @@ describe "Schools and teachers - shard choice is dependent" do
 
   describe "school" do
     it "has a teacher on the same shard" do
-      stc = @school.teachers.first.connection.real_connection.connection_name
+      stc = @school.teachers.on_default_shard.first.connection.real_connection.connection_name
       sc =  @school.connection.real_connection.connection_name
       stc.should == sc
     end
